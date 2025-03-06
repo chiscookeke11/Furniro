0778191b7b7c1e5f89964fb365478bc50e9fca32
Chinedu Okeke <chiscookeke11@gmail.com>
2025-03-06 04:05:29 +0100

    completed navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { Heart, Menu, Search, ShoppingCart, User } from 'lucide-react'
+"use client"
+import { Heart, Menu, Search, ShoppingCart, User, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [openMobileMenu, setOpenMobileMenu] = useState(false)
 
 
   const navLinks = [
@@ -53,11 +55,11 @@ const Navbar = () => {
      
      <Image src="/logo/logo.svg" alt='logo' height={100} width={100} />
 
-     <ul className=' hidden md:flex items-center gap-10 ' >
+     <ul className=' hidden lg:flex items-center gap-8 ' >
 {
   navLinks.map((navLink, index) => (
     <Link key={index} href={navLink.url} >
-      <li className='mx-2 text-base font-medium text-[#000000] ' > {navLink.label} </li>
+      <li className='mx-2 text-base font-medium text-[#000000] hover:text-[#B88E2F] transition-all duration-300 ease-in-out ' > {navLink.label} </li>
     </Link>
   ))
 }
@@ -69,9 +71,26 @@ const Navbar = () => {
         <li className={`mx-2 ${index + 1 === 4 ? "block" : "hidden md:block" } `} > {iconLink.icon} </li>
          </Link>
       ))}
-      <button className='cursor-pointer block md:hidden ' >
+      <button onClick={()=> {setOpenMobileMenu(true)}} className='cursor-pointer block lg:hidden ' >
         <Menu/>
       </button>
+     </ul>
+
+
+
+     <ul className={`w-full fixed top-0 left-0 bg-[#B88E2F] h-screen transition duration-300 ease-in-out flex flex-col gap-3.5 items-center justify-center ${openMobileMenu? "translate-y-0" : "translate-y-[-100%] "} `} >
+      <button onClick={()=> setOpenMobileMenu(false)} className='absolute top-5 right-7 cursor-pointer ' >
+        <X className='text-white ' size={30} />
+      </button>
+      {
+  navLinks.map((navLink, index) => (
+    <Link key={index} href={navLink.url} >
+      <li className='mx-2 text-base font-medium text-[#FFF3E3] hover:text-white transition-all duration-300 ease-in-out ' > {navLink.label} </li>
+    </Link>
+  ))
+}
+      
+
      </ul>
 
      
