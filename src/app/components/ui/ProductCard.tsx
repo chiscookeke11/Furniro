cbd299af6b4bb53a2d35c2f49ae68cccdafd5b95
chiscookeke11 <chiscookeke11@gmail.com>
2025-04-03 07:37:47 +0100

    fix

--- src/app/components/ui/ProductCard.tsx
+++ src/app/components/ui/ProductCard.tsx
@@ -27,7 +27,7 @@ export default function ProductCard({ image, furnitureName, description, price, 
         <div className="bg-[#F4F5F7] w-[160px] md:w-full md:max-w-[285px] md:max-h-[446px] relative flex flex-col items-stretch justify-between cursor-pointer overflow-hidden group  "  >
 
 
-            <div className=" w-full bg-green-500 h-[67.5%] relative "  >
+            <div className=" w-full h-[67.5%] relative "  >
                 <Image src={image} alt={`${furnitureName} image `} height={100} width={100} className="w-full h-full object-cover " />
 
 
@@ -47,11 +47,11 @@ export default function ProductCard({ image, furnitureName, description, price, 
 
 
             <div className="w-full h-full p-4 flex flex-col items-start justify-center gap-2 "  >
-                <h2 className="text-[#3A3A3A] font-semibold text-2xl "  >{furnitureName} </h2>
-                <p className=" text-[#898989] text-base font-medium "  >{description} </p>
-                <div className="w-full flex items-center gap-4 " >
-                    <h2 className=" text-[#3A3A3A] text-xl font-semibold  "  >Rp {newPrice === 0 ? price.toLocaleString() : newPrice.toLocaleString()} </h2>
-                    <h3 className="text-base font-normal text-[#B0B0B0] line-through " > {newPrice > 0 ? ` Rp  ${price.toLocaleString()}` : null}</h3></div>
+                <h2 className="text-[#3A3A3A] font-semibold text-xl md:text-2xl "  >{furnitureName} </h2>
+                <p className=" text-[#898989] text-sm md:text-base font-medium "  >{description} </p>
+                <div className="w-full flex flex-col md:flex-row items-start md:items-center gap-4  " >
+                    <h2 className=" text-[#3A3A3A] text-sm md:text-lg font-semibold whitespace-nowrap  "  >Rp {newPrice === 0 ? price.toLocaleString() : newPrice.toLocaleString()} </h2>
+                    <h3 className="md:text-base text-xs font-normal text-[#B0B0B0] line-through  whitespace-nowrap " > {newPrice > 0 ? ` Rp  ${price.toLocaleString()}` : null}</h3></div>
             </div>
 
 
@@ -59,13 +59,13 @@ export default function ProductCard({ image, furnitureName, description, price, 
 
             <div className="overlay absolute bottom-0 left-0 w-full h-0 bg-[#3A3A3A]/75  flex flex-col items-center justify-center gap-6  z-10 cursor-default overflow-hidden group-hover:h-full transition-all duration-400 ease-in-out ">
 
-                <button className=" bg-[#ffffff] w-full max-w-[202px] text-base font-semibold text-[#B88E2F] h-full max-h-[48px] cursor-pointer transform hover:scale-90 transition-all ease-in-out duration-150 "  >Add to cart</button>
+                <button className=" bg-[#ffffff] w-full   max-w-[202px] text-xs md:text-base font-semibold text-[#B88E2F] h-full max-h-[48px] cursor-pointer transform hover:scale-90 transition-all ease-in-out duration-150 "  >Add to cart</button>
 
-                <div className="w-full flex items-center justify-center gap-5 " >
+                <div className="w-full flex flex-col md:flex-row items-center justify-center gap-5 " >
 
-                    <button className="text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150 "   ><Share2 size={15} /> Share</button>
-                    <button className="text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150" ><ArrowDownUp size={15}/> Compare</button>
-                    <button className="text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150" ><Heart size={15}/> Like</button>
+                    <button className=" text-sm md:text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150 "   ><Share2 size={15} /> Share</button>
+                    <button className="text-sm md:text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150" ><ArrowDownUp size={15}/> Compare</button>
+                    <button className="text-sm md:text-base font-semibold text-[#ffffff] flex items-center justify-center gap-0.5 cursor-pointer hover:text-[#B88E2F] transition-all ease-in-out duration-150" ><Heart size={15}/> Like</button>
 
                 </div>
 
