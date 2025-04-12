0a6215bc0516d4708e5620e39625ad40b4b8b2c7
chiscookeke11 <chiscookeke11@gmail.com>
2025-04-12 12:49:59 +0100

    fix

--- src/app/components/RoomInspirations.tsx
+++ src/app/components/RoomInspirations.tsx
@@ -2,8 +2,8 @@
 
 
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 // import AutoPlay from './carosuel';
 
 
